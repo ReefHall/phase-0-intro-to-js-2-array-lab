63fc8a5d0da4e384ae69a7cc8f9c2dbe0e153ce7
Shareif Hall <shareifhall@yahoo.com>
2023-02-10 04:00:29 -0500

    commit tree 135d02fed5ab9235739defa86c9b600a520b5f8b

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats = ["Milo","Otis", "Garfield"];
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    
+}
+cats.destructivelyAppendCat("Ralph");
+cats.lenght = 0;
+cats.push("Milo", "Otis", "Garfield");
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+cats.destructivelyPrependCat("Bob");
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+cats.destructivelyRemoveLastCat();
+cats.lenght = 0;
+cats.push("Milo", "Otis", "Garfield")
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+cats.destructivelyRemoveFirstCat();
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function appendCat(name) {
+    const copyCats = [...cats, name];
+    return copyCats;
+}
+cats.appendCat("Broom");
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function prependCat(name) {
+    const copyCats = [name, ...cats];
+    return copyCats;
+
+}
+cats.prependCat("Arnold");
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function removeLastCat() {
+    return cats.slice(0, cats.length -1)
+}
+cats.removeLastCat();
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+function removeFirstCat() {
+    return cats.slice(1);
+}
+cats.removeFirstCat();
+
